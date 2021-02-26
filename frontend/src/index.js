df85d5824e27851d0ef6eb9b451fbc449e263f8b
khubaibali <khubaib94quadri@gmail.com>
2021-02-26 21:02:39 +0500

    hello

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import App from './App'
+import './bootstrap.min.css'
+import './index.css'
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
