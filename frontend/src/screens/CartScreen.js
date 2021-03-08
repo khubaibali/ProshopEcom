7ea5aa2ca3636ed9b22c0f921110699e56edd09b
khubaibali <76751259+khubaibali@users.noreply.github.com>
2021-03-08 12:15:47 +0500

    AddToCart functionality completed
    Add to cart functionality is completed User authentication is left in order to complete proceed to checkout

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -10,7 +10,7 @@ import {
 } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { addToCart } from "../actions/cartActions";
+import { addToCart, removeFromCart } from "../actions/cartActions";
 import Message from "../components/Message";
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id;
@@ -19,15 +19,14 @@ const CartScreen = ({ match, location, history }) => {
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
-  console.log(cart);
 
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, qty));
     }
   }, [dispatch, productId, qty]);
-  const removeFromCartHandler = () => {
-    console.log("remove");
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id));
   };
   const checkoutHandler = () => {
     console.log("checkout");
